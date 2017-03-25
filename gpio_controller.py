8d262d885b8a2987000c32d0f1d189e9d074789d
CorruptData <mccargishm@gmail.com>
2017-03-24 20:18:36 -0400

    A more visually appealing test
    Feat. AutoK1LL

--- gpio_controller.py
+++ gpio_controller.py
@@ -5,14 +5,17 @@ class GPIO_Controller:
 	def __init__(self, input_ports):
 		self.input_ports = input_ports
 		GPIO.setmode(GPIO.BCM)
+		
 		for port in input_ports:
 			GPIO.setup(port, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 		
 	def poll(self):
 		ports_on = []
+		
 		for port in input_ports:
 			if GPIO.input(port):
 				ports_on += [input_ports.index(port)]
+				
 		if len(ports_on) == 2:
 			return ports_on
 		else:
